551fc7acee93469c74515a050eb0c934c6ad72be
Rakesh_S <srakesh1996@outlook.com>
2018-09-11 11:51:48 +0530

    Create settings.h

---
+++ settings.h
@@ -0,0 +1,14 @@
+/* settings.h */
+
+#define RS RC0
+#define EN RC1
+#define D0 RB0
+#define D1 RB1
+#define D2 RB2
+#define D3 RB3
+#define D4 RB4
+#define D5 RB5
+#define D6 RB6
+#define D7 RB7
+
+#define lcd_data PORTB
